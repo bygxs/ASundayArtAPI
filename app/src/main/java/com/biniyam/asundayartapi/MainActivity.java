91b5ee31aa207ff955d46f9e311acd3ef1c71ff5
bygxs <114972065+bygxs@users.noreply.github.com>
2023-05-31 02:22:15 +0200

    31 May 2023 Tuesday
    15:01
    various trials, failures and errors to implement search now back to simple ArtworkListResponse and that list randomized locally to be displayed. But the list is the same every time, I guess i will address this later. now I am only interested in sending search query and getting the result a list of artworks by artist name queried.

--- app/src/main/java/com/biniyam/asundayartapi/MainActivity.java
+++ app/src/main/java/com/biniyam/asundayartapi/MainActivity.java
@@ -1,26 +1,20 @@
 package com.biniyam.asundayartapi;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.biniyam.asundayartapi.adapter.ArtworkAdapter;
-import com.bumptech.glide.Glide;
 import com.biniyam.asundayartapi.api.ArticApiService;
 import com.biniyam.asundayartapi.api.RetrofitClient;
 import com.biniyam.asundayartapi.response.ArtworkData;
 import com.biniyam.asundayartapi.response.ArtworkListResponse;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import retrofit2.Call;
@@ -32,42 +26,76 @@ public class MainActivity extends AppCompatActivity {
     private static final String BASE_URL = "https://api.artic.edu/";
     private RecyclerView recyclerView;
     private ArtworkAdapter artworkAdapter;
+    private List<ArtworkData> artworkList;
+
+    //private EditText searchEditText;
+    //private ImageView searchButton;
+
+    private ArticApiService apiService;
+    private String currentQuery = ""; // Stores the current search query
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //searchEditText = findViewById(R.id.searchEditText);
+        //searchButton = findViewById(R.id.searchButton);
         recyclerView = findViewById(R.id.recyclerView);
+
+        artworkList = new ArrayList<>();
+        artworkAdapter = new ArtworkAdapter(artworkList, this);
+        recyclerView.setAdapter(artworkAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        ArticApiService apiService = RetrofitClient.getClient(BASE_URL).create(ArticApiService.class);
+        // Initialize the API service
+        apiService = RetrofitClient.getClient(BASE_URL).create(ArticApiService.class);
 
-        // Make the API call to fetch a list of artwork titles and image IDs
-        Call<ArtworkListResponse> call = apiService.getArtworkList("title,image_id", 10); // Fetch 10 artworks with titles and image IDs
+        // Fetch and display random artworks on app start
+        fetchArtworkList(99);
+
+        /*
+        // Set click listener on the search button
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String query = searchEditText.getText().toString().trim();
+                //performSearch(query);
+            }
+        });
+        */
+    }
+
+    private void fetchArtworkList(int limit) {
+        Call<ArtworkListResponse> call = apiService.getArtworkList(limit, null);
         call.enqueue(new Callback<ArtworkListResponse>() {
             @Override
             public void onResponse(Call<ArtworkListResponse> call, Response<ArtworkListResponse> response) {
                 if (response.isSuccessful()) {
-                    ArtworkListResponse artworkListResponse = response.body();
-                    if (artworkListResponse != null) {
-                        List<ArtworkData> artworkList = artworkListResponse.getData();
-
-                        // Create the adapter and set it to the RecyclerView
-                        artworkAdapter = new ArtworkAdapter(artworkList, MainActivity.this);
-                        recyclerView.setAdapter(artworkAdapter);
-                    }
+                    List<ArtworkData> artworks = response.body().getData();
+                    List<ArtworkData> randomArtworks = getRandomArtworks(artworks, limit);
+                    artworkList.clear();
+                    artworkList.addAll(randomArtworks);
+                    artworkAdapter.notifyDataSetChanged();
                 } else {
-                    // Handle API error
-                    Log.e("API Error", "Response failed with code: " + response.code());
+                    // Handle API call error
                 }
             }
 
             @Override
             public void onFailure(Call<ArtworkListResponse> call, Throwable t) {
-                // Handle network or other errors
-                Log.e("API Error", "API call failed: " + t.getMessage());
+                // Handle API call failure
             }
         });
     }
+
+    private List<ArtworkData> getRandomArtworks(List<ArtworkData> artworkList, int limit) {
+        List<ArtworkData> randomArtworks = new ArrayList<>(artworkList);
+        Collections.shuffle(randomArtworks);
+        if (randomArtworks.size() > limit) {
+            randomArtworks = randomArtworks.subList(0, limit);
+        }
+        return randomArtworks;
+    }
+
 }
--- app/src/main/java/com/biniyam/asundayartapi/response/ArtworkListResponse.java
+++ app/src/main/java/com/biniyam/asundayartapi/response/ArtworkListResponse.java
@@ -12,17 +12,3 @@ public class ArtworkListResponse {
 }
 
 
-/*
-public class ArtworkResponse {
-    private ArtworkData data;
-    private ConfigData config;
-
-    public ArtworkData getData() {
-        return data;
-    }
-
-    public ConfigData getConfig() {
-        return config;
-    }
-}
-*/
